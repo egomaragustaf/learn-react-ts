b59aa289e7f452e62c3314785e96d3ebd298edd1
Ego Maragustaf <egomaragustaf@gmail.com>
2023-07-12 20:23:07 +0700

    feat: add about component

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+import Button from "./Button";
+
+export default function About() {
+  return (
+    <div>
+      <h1>History</h1>
+      <p>This is history of this e-commerce</p>
+      <Button>Back To Home</Button>
+    </div>
+  );
+}
