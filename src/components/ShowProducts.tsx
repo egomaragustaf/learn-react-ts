bf518a677d5f264dfea4a034e5b648b88fe86b70
Ego Maragustaf <egomaragustaf@gmail.com>
2023-07-14 00:50:33 +0700

    style: styling card product

--- src/components/ShowProducts.tsx
+++ src/components/ShowProducts.tsx
@@ -2,9 +2,8 @@ import { useEffect, useState } from "react";
 
 type Product = {
   brand: string;
-  thumbnail: string;
+  image: string;
   title: string;
-  description: string;
   price: string;
   id: number;
   category: string;
@@ -16,13 +15,12 @@ const ShowProducts: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch("https://dummyjson.com/products")
+    fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
       .then(
         (result) => {
           setIsLoaded(true);
-          setProducts(result.products);
-          console.log(result);
+          setProducts(result);
         },
         (error) => {
           setIsLoaded(true);
@@ -31,22 +29,26 @@ const ShowProducts: React.FC = () => {
       );
   }, []);
 
-  if (error) {
+  if (error && products.length === 0) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
   } else {
     return (
-      <div className="flex flex-col gap-4 max-w-sm">
+      <div className="flex flex-row flex-wrap w-3/4 gap-4 font-semibold dark:text-black text-white">
         {products.map((product) => (
-          <ul key={product.id}>
-            <li>{product.thumbnail}</li>
-            <li>Title: {product.title}</li>
-            <li>Description: {product.description}</li>
-            <li>Brand: {product.brand}</li>
-            <li>Category: {product.category}</li>
-            <li>Price: ${product.price}</li>
-          </ul>
+          <div className="w-60 bg-slate-200 hover:bg-slate-300 rounded-md shadow mx-2 my-2 flex flex-col justify-between p-4">
+            <ul key={product.id}>
+              <img
+                src={product.image}
+                className="rounded-t-lg h-40 w-full object-cover"
+              />
+              <li>Title: {product.title}</li>
+              <li>Brand: {product.brand}</li>
+              <li>Category: {product.category}</li>
+              <li>Price: ${product.price}</li>
+            </ul>
+          </div>
         ))}
       </div>
     );
