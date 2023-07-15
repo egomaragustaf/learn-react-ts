db53b5342d7d144049fbef96315f3021defc022f
Ego Maragustaf <egomaragustaf@gmail.com>
2023-07-16 00:03:52 +0700

    feat: add search form in show products page

--- src/components/ShowProducts.tsx
+++ src/components/ShowProducts.tsx
@@ -42,19 +42,21 @@ const ShowProducts: React.FC = () => {
   } else {
     return (
       <div className="dark:text-black text-white">
-        <label
-          htmlFor="searchProduct"
-          className="dark:text-white text-black mx-2 my-2">
-          Search Product
-        </label>
-        <input
-          value={search}
-          onChange={(e) => handleChangeSearch(e.target.value)}
-          type="search"
-          name="searchProduct"
-          id="searchProduct"
-          className="px-2 py-2 mx-2 my-2 rounded"
-        />
+        <form id="searchForm">
+          <label
+            htmlFor="searchProduct"
+            className="dark:text-white text-black mx-2 my-2">
+            Search Product
+          </label>
+          <input
+            value={search}
+            onChange={(e) => handleChangeSearch(e.target.value)}
+            type="search"
+            name="searchProduct"
+            id="searchProduct"
+            className="px-2 py-2 mx-2 my-2 rounded"
+          />
+        </form>
         <div className="flex flex-row flex-wrap w-full gap-4 font-semibold ">
           {products.map((product) => (
             <div
