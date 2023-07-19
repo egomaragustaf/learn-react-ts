0359b921cb836dc69bfffc91c868b73f1edb9f2d
Ego Maragustaf <egomaragustaf@gmail.com>
2023-07-19 22:19:20 +0700

    style: styling and layouting home page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,8 +3,10 @@ import { Layout } from "../components";
 export default function HomeRoute() {
   return (
     <Layout>
-      <h1>Hello! I am Ego Maragustaf</h1>
-      <p>This is Epic Store</p>
+      <div className="w-full flex flex-col justify-center items-center text-4xl gap-8">
+        <h1>Hello! I am Ego Maragustaf</h1>
+        <p>This is Epic Store</p>
+      </div>
     </Layout>
   );
 }
