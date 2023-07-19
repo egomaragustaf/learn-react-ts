eeb454961dbcb404ea759dc8fd5ad78ad2fa7b6a
Ego Maragustaf <egomaragustaf@gmail.com>
2023-07-19 22:20:54 +0700

    style: styling and layouting about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,15 +1,12 @@
-import { Link } from "react-router-dom";
-
-import { Button, Layout } from "../components";
+import { Layout } from "../components";
 
 export default function AboutRoute() {
   return (
     <Layout>
-      <h1>History</h1>
-      <p>This is history of this e-commerce</p>
-      <Button>
-        <Link to="/">Back To Home</Link>
-      </Button>
+      <div className="w-full flex flex-col justify-center items-center text-4xl gap-8">
+        <h1>History</h1>
+        <p>This is history of this e-commerce</p>
+      </div>
     </Layout>
   );
 }
